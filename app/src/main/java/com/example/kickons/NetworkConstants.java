3bf5806acb8336f61e6d9de07d32fd5e0715c20c
shaun <shaungumbaketi@gmail.com>
2021-06-01 09:28:43 +1000

    added a hostaddres to network constatants for easy operability between different work env using different IPs

--- app/src/main/java/com/example/kickons/NetworkConstants.java
+++ app/src/main/java/com/example/kickons/NetworkConstants.java
@@ -2,11 +2,13 @@ package com.example.kickons;
 
 public class NetworkConstants {
 
-    public static String SERVER_URL = "http://192.168.8.104:8000/kickons_inventory/users/1/?format=json"; //TEST CHANGE TO A DYNAMIC
-    public static String SERVER_POST_URL = "http://192.168.8.104:8000/kickons_inventory/users/";
-    public static String SERVER_LOG_IN = "http://192.168.8.104:8000/kickons_inventory/login/";
-    public static String SERVER_LOG_IN_VERIFICATION = "http://192.168.8.104:8000/kickons_inventory/login/verification/";
-   public static String SERVER_GET_ITEMS =  "http://192.168.8.104:8000/kickons_inventory/items/";
+    static String hostAddrTestServer = "192.168.1.102";
+
+    public static String SERVER_URL = "http://"+hostAddrTestServer +":8000/kickons_inventory/users/1/?format=json"; //TEST CHANGE TO A DYNAMIC
+    public static String SERVER_POST_URL = "http://"+hostAddrTestServer +":8000/kickons_inventory/users/";
+    public static String SERVER_LOG_IN = "http://"+hostAddrTestServer +":8000/kickons_inventory/login/";
+    public static String SERVER_LOG_IN_VERIFICATION = "http://"+hostAddrTestServer +":8000/kickons_inventory/login/verification/";
+   public static String SERVER_GET_ITEMS =  "http://"+hostAddrTestServer +":8000/kickons_inventory/items/";
 
 
     public NetworkConstants() {
