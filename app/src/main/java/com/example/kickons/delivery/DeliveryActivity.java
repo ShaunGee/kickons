a9f10604d8570dc3726ebecef922417a7c3272d6
shaun <shaungumbaketi@gmail.com>
2021-06-09 15:35:09 +1000

    adding a delivery feature so a deliverer can log into app and choose a delivery to do

---
+++ app/src/main/java/com/example/kickons/delivery/DeliveryActivity.java
@@ -0,0 +1,22 @@
+package com.example.kickons.delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.example.kickons.R;
+
+public class DeliveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery);
+
+        FrameLayout frameLayout = findViewById(R.id.delivery_activity_frame_layout);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/kickons/delivery/DeliveryDetails.java
@@ -0,0 +1,4 @@
+package com.example.kickons.delivery;
+
+public class DeliveryDetails {
+}
